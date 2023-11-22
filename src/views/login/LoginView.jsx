9f7d381a4a34611cab64ccff474af4aa3ce7499b
albertcastineira <albertcastiaran@gmail.com>
2023-11-22 19:50:00 +0100

    add views

--- src/views/login/LoginView.jsx
+++ src/views/login/LoginView.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import background from '../../assets/images/loginBg.png'
 import logoImg from '../../assets/images/logo.png'
+import logisticsImg from '../../assets/images/logisticsImg.svg'
 
 export default function LoginView() {
   return (
     <>
       <div className="login absolute w-full h-full bg-red-400 bg-cover p-4" style={{backgroundImage: `url(${background})`}}>
-        <img src={logoImg} className="w-[8em]" alt="" />
-        <div className="px-5">
-          <p className="text-[#C0C0C0] font-bold">
-            Welcome back to <span className="text-black">Your</span> <span class="text-[#E39945]">Warehouse</span>
-          </p>
+        <div className="grid grid-cols-2">
+          <div>
+              <img src={logoImg} className="w-[8em]" alt="" />
+            <div className="px-5">
+              <p className="text-[#C0C0C0] font-bold">
+                Welcome back to <span className="text-black">Your</span> <span class="text-[#E39945]">Warehouse</span>
+              </p>
+              
+            </div>
+          </div>
+          <div>
+            <img src={logisticsImg} className="w-[30em]" alt="" />
+          </div>
         </div>
+        
       </div>
     </>
   )
